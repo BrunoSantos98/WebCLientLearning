60067e38ffc05664713a1fdad9d26bb9507286cf
Bruno Santos <bruno4t@hotmail.com>
2023-04-26 11:11:02 -0300

    FIX: correção no lançamento de exceções
    Adição de getter e setter na class standard error para poder l;ançar corretamente as exceções

--- webclientdevtoolscustomer/src/main/java/com/webclientdevtools/webclientdevtoolscustomer/services/implementation/handleErrors/ServiceExceptionHandler.java
+++ webclientdevtoolscustomer/src/main/java/com/webclientdevtools/webclientdevtoolscustomer/services/implementation/handleErrors/ServiceExceptionHandler.java
@@ -5,12 +5,12 @@ import com.webclientdevtools.webclientdevtoolscustomer.exception.UserNotFoundExc
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ServiceExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<StandardError> UserNotFound(UserNotFoundException e, HttpServletRequest request){
